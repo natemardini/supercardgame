3017958391f1f1216f47f1832d3c141c446a110d
zhonghaoliu <joshualiu94@gmail.com>
2017-10-29 01:01:54 +0000

    add user2 page, add startgame button and animations

--- routes/home.js
+++ routes/home.js
@@ -17,5 +17,12 @@ module.exports = (passport) => {
         res.redirect("/html/game.html");
     });
 
+    /**
+     * GET /game_user2.html
+     */
+    router.get("/html2", (req, res) => {
+        res.redirect("/html/game_user2.html");
+    });
+
     return router;
 };
