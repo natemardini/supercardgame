56d93c696ccf845a255306cef864828496be6d9e
Lex Yehia <lex.yehia@icloud.com>
2017-10-29 23:23:10 -0400

    Styling fixes

--- logic/goofspiel.js
+++ logic/goofspiel.js
@@ -2,64 +2,6 @@ const _ = require("lodash");
 const { Deck } = require("./../logic/card");
 
 
-
-/**
- * function: create an array of 13 elements
- */
-// function createSuit () {
-//     const suit = [];
-//     for (let i = 1; i <= 13; i++) {
-//         suit.push(i);
-//     }
-//     return suit;
-// }
-
-/**
- * function: shuffle an array
- */
-// function shuffle (input) {
-//     for (let i = input.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [input[i], input[j]] = [input[j], input[i]];
-//     }
-//     return input;
-// }
-
-
-// let prizes = createSuit();  // from Game -> cards_in_play
-// let user1 = createSuit();   // from Game -> cards_in_play
-// let user2 = createSuit();   // from Game -> cards_in_play
-
-// let user1Score = 0; // from db Match -> score
-// let user2Score = 0; // from db Match -> score
-
-// prizes = shuffle(prizes);
-// user1 = shuffle(user1);
-// user2 = shuffle(user2);
-
-// console.log(`prizes: ${prizes}`);
-// console.log(`user1: ${user1}`);
-// console.log(`user2: ${user2}`);
-
-// prizes: [3,7,10,12,1,11,9,2,5,6,4,13,8];
-// user1: [3,8,10,4,11,6,5,13,2,7,12,9,1];
-// user2: [1,3,5,4,2,10,12,6,11,9,7,13,8];
-
-
-// game start
-
-/**
- * function: startGame,
- * input should be an object, ie:
- * input = {round: 12,
-    user1score: 43,
-    user2score: 4,
-    user1win: 1,
-    user2win: 0,
-    prize: 12,
-    user1card: 10,
-    user2card: 8 }
- */
 function calculate(game, user, input) {
 
     if (game.status === 3) {
@@ -82,38 +24,6 @@ function calculate(game, user, input) {
     return [game, winner];
 }
 
-
-//     // if a user's score is > 45.5, win the game
-//     else if (player1.score > player2.score && player1.score >= 45.5) {
-//         player1.win = true;       //game finished,  user1 win
-//     }
-//     else if (player2.score > player1.score && player2.score >= 45.5) {
-//         player2.win = true;      //game finished, user2 win
-//     }
-
-//     else {
-//         if (user1Card > user2Card) {
-//             user1Score += prize;
-//         }
-//         else if (user1Card < user2Card) {
-//             user2Score += prize;
-//         }
-//         else {}  // console.log(`TIE this round!`);
-
-//         if (user1Score > user2Score && user1Score >= 45.5) {
-//             user1Win = 1;
-//         }
-//         else if (user2Score > user1Score && user2Score >= 45.5) {
-//             user2Win = 1;
-//         }
-//         else {}
-//     }
-
-//     return { user1score: user1Score,
-//         user2score: user2Score,
-//         user1win: user1Win,
-//         user2win: user2Win };
-// }
 
 function checkBids(game, prize) {
     prize = _.find(game.deck.prize, { valueN: prize.value, suit: prize.suit });
@@ -184,7 +94,6 @@ function cleanUp(game, prize) {
 }
 
 function placeBid(user, game, bid) {
-    //const currentPlayer = _.find(game.players, { "userId": ObjectId(user) });
     const currentPlayer = _.find(game.players, { "userId": user._id });
     const bidCard = _.find(currentPlayer.hand, { valueN: bid.value, suit: bid.suit });
 
